4696a5154d45b2346477b2969e19674176eb4ef9
Valery Semenenko <gearmobile@gmail.com>
2023-03-02 18:52:29 +0300

    private static members

---
+++ Static_Private_Member/src/Simple.java
@@ -0,0 +1,11 @@
+public class Simple {
+    private static int age = 20;
+
+    public static int getAge() {
+        return age;
+    }
+
+    public static void setAge(int value) {
+        age = value;
+    }
+}
